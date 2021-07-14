3379c58ec2211cd3786ba60dba72409d410a5faa
Hassan T Ali <hassantarekha@gmail.com>
2021-07-14 02:46:13 +0200

    solve a bug : action.type

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import { createStore } from 'redux'
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if (action === 'increment') {
+    if (action.type === 'increment') {
         return {
             counter: state.counter + 1
         }
     }
 
-    if (action === 'decrement') {
+    if (action.type === 'decrement') {
         return {
             counter: state.counter - 1
         }
