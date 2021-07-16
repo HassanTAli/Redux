125cffcdd77bcc7cf2cc0904b09d56e46361d371
Hassan T Ali <hassantarekha@gmail.com>
2021-07-16 02:40:54 +0200

    add toggle function

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,36 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const intialState = {
+    counter: 0,
+    showCounter: true
+}
+
+const counterReducer = (state = intialState, action) => {
     if (action.type === 'increment') {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'increase') {
         return {
-            counter: state.counter + action.amount
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'decrement') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
 
