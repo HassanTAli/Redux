42a5996a2208c9bbbc0602063c47f9b8e41ada4e
Hassan T Ali <hassantarekha@gmail.com>
2021-07-21 21:06:06 +0200

    add isAuth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Header.module.css';
 
 const Header = () => {
+  const isAuth = useSelector(state => state.auth.isAuthenticed)
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
+      {isAuth && <nav>
         <ul>
           <li>
             <a href='/'>My Products</a>
@@ -16,7 +20,8 @@ const Header = () => {
             <button>Logout</button>
           </li>
         </ul>
-      </nav>
+      </nav>}
+
     </header>
   );
 };
