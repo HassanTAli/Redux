e2add6a72a28a869817d025d4dff80f2e95b4d3a
Hassan T Ali <hassantarekha@gmail.com>
2021-07-21 21:20:53 +0200

    add loginHandler

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
 import classes from './Auth.module.css';
 
 const Auth = () => {
+  const disPatch = useDispatch()
+
+  const loginHandler = (event) => {
+    event.preventDefault()
+
+    disPatch(authActions.login())
+  }
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
