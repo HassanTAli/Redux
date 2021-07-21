8c11145f30c9dca364450b5c1b48b49dd270e0ef
Hassan T Ali <hassantarekha@gmail.com>
2021-07-21 20:20:11 +0200

    add more components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
-
+import Auth from './components/Auth'
+import Header from './components/Header'
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
   );
 }
 
